c337a16deeeb8898582f50fecc2e553defa5f8c0
kAiF <209830895+askthekaif@users.noreply.github.com>
2025-05-04 09:19:57 +0000

    sky: Initialize for Infinity-X v2.x

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sky.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_sky-bp1a-eng \
-    lineage_sky-bp1a-userdebug \
-    lineage_sky-bp1a-user
+    $(LOCAL_DIR)/infinity_sky.mk
--- infinity_sky.mk
+++ infinity_sky.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common Infinity-X configurations
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/lineage/config/BoardConfigReservedSize.mk)
+$(call inherit-product, vendor/infinity/config/BoardConfigReservedSize.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -23,16 +23,16 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
-# Evolution-X Flags
+# Infinity-X Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GMS := true
+INFINITY_MAINTAINER := "kAiF"
+WITH_GAPPS := true
+TARGET_SHIPS_FULL_GAPPS := true
 USE_PIXEL_CHARGER := true
-TARGET_INCLUDE_ACCORD := false
-BUILD_BCR := true
 
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := lineage_sky
+PRODUCT_NAME := infinity_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
